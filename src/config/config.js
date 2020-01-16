cd31de4cb5c7832775a5ea71239b73bffd0fde7b
jackson <lealjackson@hotmail.com>
2020-01-16 09:29:19 -0300

    Arrumando a bagunça

--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/no-dynamic-require */
-const Config = require(`./env/${(process.env.NODE_ENV || 'development')}.js`);
-
-
-module.exports = Config;
--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const usuariosRoute = require('./users');
-
-const router = express.Router();
-
-router.use('/users', usuariosRoute);
-router.get('/', (req, res) => res.send('App Online!'));
-
-module.exports = router;
