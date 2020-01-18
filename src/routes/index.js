4c2809921668187144211116bba273476c215946
jackson <lealjackson@hotmail.com>
2020-01-18 12:27:59 -0300

    Organizando os diretorios de acordo com o livro by jack

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import usersRoute from './users';
-
-const router = express.Router();
-
-router.use('/users', usersRoute);
-router.get('/', (req, res) => res.send('Hello World!'));
-
-export default router;
