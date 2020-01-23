8207ad34bb37690b1d19843430edf9c3a1e09c97
jackson <lealjackson@hotmail.com>
2020-01-23 14:40:19 -0300

    fazendo testes

--- dist/controllers/login.js
+++ dist/controllers/login.js
@@ -57,7 +57,7 @@ class LoginController {
         email: email,
         password: password
       });
-      res.sendStatus(login);
+      res.send(login);
     } catch (err) {
       res.status(400).send(err.message);
     }
